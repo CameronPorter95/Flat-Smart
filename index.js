78c46ca26836305be26857296355dcf1b0485521
naggyman <morgan@french.net.nz>
2017-08-13 09:11:45 +1200

    add link to pug file for suburb data

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 //Set the template engine
 app.set('view engine', 'pug');
 //Enable static file serving
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+var suburbs = [];
+var getSuburb = function(){
+	fs.readFile("suburbs.json", 'utf8', function(err,data){
+		if(err) throw err;
+		suburbs = JSON.parse(data);
+	})
+};
 //Checks if the app is running on Heroku
 if(process.env.NODE && ~process.env.NODE.indexOf("heroku")){
 	//Do basic HTTP server setup, Heroku will handle HTTPS
@@ -23,9 +30,10 @@ else {
 };
 
 app.get('/', function (req, res) {
+	getSuburb();
   //res.render(<pug file>, parameters: {title: <page title>, resultsName: <Title for results table>});
   res.render('index', {title: 'Search',
   	resultsName: 'Test Table:',
-  	suburbsArray: 
+  	suburbsArray: suburbs
   });
 });
