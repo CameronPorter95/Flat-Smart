ddb3ea0f21d31edf912d7969fe2531a0ac11275d
GregoryMason <GregoryMason@users.noreply.github.com>
2017-08-12 16:00:54 +1200

    Enabling CSS and fancies

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   //res.send('Hello World!');
