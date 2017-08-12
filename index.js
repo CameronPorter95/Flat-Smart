28f915bf8b0b8695d7760821ed060e030af9e8a8
Li Li <walkcoolboyli@gmail.com>
2017-08-12 14:00:07 +1200

    support heroku

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+//Checks if the app is running on Heroku
+if(process.env.NODE && ~process.env.NODE.indexOf("heroku")){
+	//Do basic HTTP server setup, Heroku will handle HTTPS
+	port = process.env.PORT || 8080;
+	app.listen(port, function () {
+ 	console.log('App listening on port '+port);
+
+});}
+else {
+ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
+ });
+};
